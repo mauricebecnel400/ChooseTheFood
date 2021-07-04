e50182b62b8c4c3b1c8c5f033e864d680b2036e9
Maurice Becnel <mauricebecnel400@gmail.com>
2021-07-04 15:52:16 -0500

    removed console.log

--- functions/getRandomFoodData.js
+++ functions/getRandomFoodData.js
@@ -5,12 +5,12 @@ const client = yelp.client(process.env.YELP_FUSION_API_KEY);
 
 exports.handler = async event => {
     try {
-        console.log(event.body);
         const requestBody = JSON.parse(event.body);
         console.log('processing request with query body: ', typeof requestBody);
 
         let response = await client.search(requestBody);
         if (response && response.body) {
+            console.log('hit');
             if (response.statusCode < 300) {
                 console.log('Sucessfully retrieved data for search query: ', requestBody, 
                 'with results: ', response.body);
